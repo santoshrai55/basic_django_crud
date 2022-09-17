16d8f907e9fec5f4b645e75146a9602c6a35c05f
Santosh Rai <rai.santosh55@gmail.com>
2022-09-17 23:46:50 +0545

    adds html for additem view

--- mini_app/urls.py
+++ mini_app/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('edit/<int:pk>', views.edit, name='edit'),
     path('login', views.userLogin, name='login'),
+    path('addItem', views.addItem, name='add'),
     path('loggedOut', views.userLogout, name='logout'),
     path('deleted/<int:pk>', views.itemDelete, name='delete'),
+
 ]
