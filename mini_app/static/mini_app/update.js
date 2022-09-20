d9ca66af3762ea3b87419abe567462f9340f3cfa
Santosh Rai <rai.santosh55@gmail.com>
2022-09-20 14:29:03 +0545

    further validation added

--- mini_app/static/mini_app/update.js
+++ mini_app/static/mini_app/update.js
@@ -1,10 +1,10 @@
 const first_name = document.getElementById("name_first").innerText;
 const last_name = document.getElementById("name_last").innerText;
 const myemail = document.getElementById("myemail").innerText;
-console.log("It is working");
-console.log(first_name);
-console.log(last_name);
-console.log(myemail);
+// console.log("It is working");
+// console.log(first_name);
+// console.log(last_name);
+// console.log(myemail);
 const btn = document.getElementById("mybtn");
 const frst = document.getElementById("first_name");
 const last = document.getElementById("last_name");
@@ -20,7 +20,6 @@ email_new.addEventListener("keypress", (event) => {
   if (email_new.length === 0) {
     btn.classList.add("disabled");
     if (label_email.childElementCount === 0) {
-      console.log("It doesn't have a Child");
       const para = document.createElement("span");
       para.innerHTML = " This cant by empty!";
       para.classList.add("text-danger");
@@ -29,6 +28,9 @@ email_new.addEventListener("keypress", (event) => {
     }
   } else if (email_new.includes("@") === false) {
     btn.classList.add("disabled");
+    if (label_email.querySelector("#label_info") !== null) {
+      label_email.removeChild(label_email.firstElementChild);
+    }
     if (label_email.childElementCount === 0) {
       const para = document.createElement("span");
       para.innerHTML = " An email must contain @ !";
@@ -40,7 +42,7 @@ email_new.addEventListener("keypress", (event) => {
     if (label_email.querySelector("#label_info") !== null) {
       label_email.removeChild(label_email.firstElementChild);
     }
-    console.log("Good Job");
+    // console.log("Good Job");
     btn.classList.remove("disabled");
   }
 });
@@ -51,19 +53,19 @@ frst.addEventListener("keypress", (event) => {
   if (firstname_new.length === 0) {
     btn.classList.add("disabled");
     if (label_first.childElementCount === 0) {
-      console.log("It doesn't have a Child");
+      //   console.log("It doesn't have a Child");
       const para = document.createElement("span");
       para.innerHTML = " This cant by empty!";
       para.classList.add("text-danger");
       document.getElementById("firstname_label").appendChild(para);
     }
 
-    console.log("btn has been disabled");
+    // console.log("btn has been disabled");
   } else {
     if (label_first.hasChildNodes()) {
       label_first.removeChild(label_first.firstElementChild);
     }
-    console.log("Good Job");
+    // console.log("Good Job");
     btn.classList.remove("disabled");
   }
 });
@@ -74,19 +76,19 @@ last.addEventListener("keypress", (event) => {
   if (lastname_new.length === 0) {
     btn.classList.add("disabled");
     if (label_last.childElementCount === 0) {
-      console.log("It doesn't have a Child");
+      //   console.log("It doesn't have a Child");
       const para = document.createElement("span");
       para.innerHTML = "This cant by empty!";
       para.classList.add("text-danger");
       document.getElementById("lastname_label").appendChild(para);
     }
 
-    console.log("btn has been disabled");
+    // console.log("btn has been disabled");
   } else {
     if (label_last.hasChildNodes()) {
       label_last.removeChild(label_last.firstElementChild);
     }
-    console.log("Good Job");
+    // console.log("Good Job");
     btn.classList.remove("disabled");
   }
 });
